dd611824e9971850bb689ad3a75932abc01532f0
Joe Benassi <joebenassi@gmail.com>
2014-11-10 20:05:58 -0600

    Trying to fix crashing by changing formatting

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,7 +1,7 @@
 var addressKey = "address";
 
 var heartRateServiceUuid = "180d";
-var humidityServiceUuid = "F000AA20-0451-4000-B000-00000000";
+var humidityServiceUuid = "F000AA2004514000B00000000000";
 var heartRateMeasurementCharacteristicUuid = "2a37";
 var clientCharacteristicConfigDescriptorUuid = "2902";
 var batteryServiceUuid = "180f";
