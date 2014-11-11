134aee40141d80e39839f471423ad6b6cf1e361f
Joe Benassi <joebenassi@gmail.com>
2014-11-11 01:09:56 -0600

    Testing more accurate scenario

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -124,8 +124,8 @@ function connectSuccess(obj)
     logData("Connected to : " + obj.name + " - " + obj.address);
 
     clearConnectTimeout();
-    tempDisconnectDevice();
-    //readHumidity(); //important
+    //tempDisconnectDevice();
+    readHumidity(); //important
   }
   else if (obj.status == "connecting")
   {
@@ -473,10 +473,12 @@ function readSuccess(obj)
         var bytes = bluetoothle.encodedStringToBytes(obj.value);
         logData("Battery level: " + bytes[0]);
         
+        /*
         logData("Subscribing to heart rate for 5 seconds");
         var paramsObj = {"serviceUuid":heartRateServiceUuid, "characteristicUuid":heartRateMeasurementCharacteristicUuid};
         bluetoothle.subscribe(subscribeSuccess, subscribeError, paramsObj);
         setTimeout(unsubscribeDevice, 5000);
+        */
     }
     else
   {
